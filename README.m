225ce86c6afaacc696968d5c9d90105cc69d9833
Lintopay <93862129+Lintopay@users.noreply.github.com>
2021-11-07 12:39:22 +0100

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -1,9 +1,6 @@
 - 👋 Hi, I’m @Lintopay
 - 👀 I’m interested in coding, UI/UX design
 - 🌱 I'm a web developer ...
-- 💞️ I’m looking to collaborate 
-- 📫 How to reach me Whatsapp +2349033666426
-
 <!---
 Lintopay/Lintopay is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
